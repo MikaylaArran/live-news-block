74630a6b1e09791795c034aa7fd14bbab4874022
MikaylaArran <62945920+MikaylaArran@users.noreply.github.com>
2026-02-14 16:02:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const CHANNELS = [
+  {
+    id: "skynews",
+    label: "SKY NEWS",
+    embedUrl: "https://www.youtube.com/embed/9Auq9mYxFEE?autoplay=1&mute=1&playsinline=1"
+  },
+  {
+    id: "nbcnews",
+    label: "NBC NEWS",
+    embedUrl: "https://www.youtube.com/embed/UCeY0bbntWw?autoplay=1&mute=1&playsinline=1"
+  },
+  {
+    id: "cbsnews",
+    label: "CBS NEWS",
+    embedUrl: "https://www.youtube.com/embed/yfJ8x0k0iDw?autoplay=1&mute=1&playsinline=1"
+  },
+  {
+    id: "abcnews",
+    label: "ABC NEWS",
+    embedUrl: "https://www.youtube.com/embed/w_Ma8oQLmSM?autoplay=1&mute=1&playsinline=1"
+  },
+  {
+    id: "dw",
+    label: "DW NEWS",
+    embedUrl: "https://www.youtube.com/embed/NcdBvZ5f4eA?autoplay=1&mute=1&playsinline=1"
+  },
+  {
+    id: "aljazeera",
+    label: "AL JAZEERA",
+    embedUrl: "https://www.youtube.com/embed/-upyPouRrB8?autoplay=1&mute=1&playsinline=1"
+  },
+  {
+    id: "france24",
+    label: "FRANCE 24",
+    embedUrl: "https://www.youtube.com/embed/l8PMl7tUDIE?autoplay=1&mute=1&playsinline=1"
+  },
+  {
+    id: "euronews",
+    label: "EURONEWS",
+    embedUrl: "https://www.youtube.com/embed/sPgqEHsONK8?autoplay=1&mute=1&playsinline=1"
+  }
+];
